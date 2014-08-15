359b8235ea9e1aae17fb59913046ea54ab83c73b
Hasan Ibna Akbar <bd.pothik@gmail.com>
2014-08-15 10:26:58 +0600

    Chapter 1 Practice
    Only Scripts are available. Maximum practices were completed in
    interactive interpreter.

---
+++ Chapter_1/hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+print "Hello, world"
+
+# Get the user's name:
+name = raw_input("What is your name? ")
+print "Hello, " + name + "!"
+raw_input("Press <enter>")
---
+++ Chapter_1/long_strings.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+print '''Single quote : This is a very long string.
+It continues here.
+And it's not over yet.
+"Hello, world!"
+Still here.'''
+
+print """Double quote : This is a very long string.
+It continues here.
+And it's not over yet.
+"Hello, world!"
+Still here."""
+
+print "This is a very long string. \
+It continues here. \
+And it's not over yet. \
+\"Hello, world!\" \
+Still here."
+
+print '''This is a very long string. \
+It continues here. \
+And it's not over yet. \
+"Hello, world!" \
+Still here.'''
